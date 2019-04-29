9102b35464e265d556fb39e7ca721c61c2f922c7
yclooper <yclooper@163.com>
2019-04-30 00:29:46 +0800

    add provider and consumer

--- spring-consumer/src/main/java/com/yclooper/springconsumer/HelloRemote.java
+++ spring-consumer/src/main/java/com/yclooper/springconsumer/HelloRemote.java
@@ -8,5 +8,5 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface HelloRemote {
 
     @RequestMapping("/hello")
-    public String hello(@RequestParam(value = "name") String name);
+    String hello(@RequestParam(value = "name") String name);
 }
