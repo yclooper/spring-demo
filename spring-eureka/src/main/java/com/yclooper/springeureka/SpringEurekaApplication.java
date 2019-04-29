2a159a10356f40a1908d3af4f932dda09b2edd98
yclooper <yclooper@163.com>
2019-04-29 17:52:57 +0800

    Initial commit

---
+++ spring-eureka/src/main/java/com/yclooper/springeureka/SpringEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.yclooper.springeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEurekaApplication.class, args);
+    }
+
+}
