c212018a7d8533ad6efc171f41f192aa9436b610
yclooper <yclooper@163.com>
2019-05-05 20:27:05 +0800

    add provider and consumer

---
+++ spring-provider/src/main/java/com/yclooper/springprovider/SpringProviderApplication.java
@@ -0,0 +1,15 @@
+package com.yclooper.springprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SpringProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProviderApplication.class, args);
+    }
+
+}
