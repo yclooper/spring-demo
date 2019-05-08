cc3646c5af3afc10d970ec9419dcf4f7027afa24
yclooper <yclooper@163.com>
2019-05-08 17:20:21 +0800

    commit init

--- spring-provider/src/main/java/com/yclooper/springprovider/TestController.java
+++ spring-provider/src/main/java/com/yclooper/springprovider/TestController.java
@@ -1,13 +1,16 @@
 package com.yclooper.springprovider;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @Value("${server.port}")
+    private String port;
     @RequestMapping("/hello")
     public String hello(String name) {
-        return "this provider return:" + name;
+        return "this provider return: " + port ;
     }
 }
---
+++ spring-zuul/src/main/java/com/yclooper/springzuul/SpringZuulApplication.java
@@ -0,0 +1,17 @@
+package com.yclooper.springzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class SpringZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringZuulApplication.class, args);
+    }
+
+}
