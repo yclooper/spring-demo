9406727228077661d7a9a4f37aad5177eb8b82b9
yclooper <yclooper@163.com>
2020-07-09 17:33:12 +0800

    commit init

--- spring-provider/src/main/java/com/yclooper/springprovider/TestController.java
+++ spring-provider/src/main/java/com/yclooper/springprovider/TestController.java
@@ -1,6 +1,8 @@
 package com.yclooper.springprovider;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
