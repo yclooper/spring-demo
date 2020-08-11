c01612a24e57cebcbe99f269a3c45ad02505286a
yclooper <yclooper@163.com>
2020-08-11 15:14:05 +0800

    init

---
+++ spring-rest-doc/src/main/java/com/yclooper/springrestdoc/SpringRestDocApplication.java
@@ -0,0 +1,13 @@
+package com.yclooper.springrestdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestDocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestDocApplication.class, args);
+    }
+
+}
